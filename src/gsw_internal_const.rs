7d8d57b2bb6e54e133995918540ef958b6b3a010
Guilherme Castelão <guilherme@castelao.net>
2021-04-02 09:45:48 -0700

    feat: Adding constants using the feature truncated
    Using a feature truncated allows to force the hardcoded truncated value
    used by other implementations thus permiting to validate our procedures
    with their values.

--- src/gsw_internal_const.rs
+++ src/gsw_internal_const.rs
@@ -2,3 +2,21 @@
 
 /// Conversion factor deciBar to Pascal
 pub const DB2PA: f64 = 1.0e4;
+
+/// SSO: Standard Ocean Reference Salinity [ g/kg ].
+pub const GSW_SSO: f64 = 35.16504;
+pub const GSW_SQRTSSO: f64 = 5.930011804372737;
+
+/// uPS: unit conversion factor for salinities [ g/kg ]
+pub const GSW_UPS: f64 = GSW_SSO / 35.0;
+
+#[cfg(feature = "truncated")]
+/// Other implementations hardcoded some constants truncating its values
+/// sfac  =  1/(40*gsw_ups) ~ 0.0248826675584615
+pub const GSW_SFAC: f64 = 0.0248826675584615;
+
+#[cfg(not(feature = "truncated"))]
+/// sfac  =  1/(40*gsw_ups) = 0.024882667558461472
+/// Two extra digits on precision compared with other implementations. The
+/// difference should be negligible but it impacts the validation tests.
+pub const GSW_SFAC: f64 = 1.0 / (40.0 * GSW_UPS);
