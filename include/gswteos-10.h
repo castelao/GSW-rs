2dee3a7ef71169c134949a626ba7b4bc28fdaf0d
Guilherme Castelão <guilherme@castelao.net>
2021-04-03 13:18:16 -0700

    Figuring out the package structure (#1)
    * feat: Implementing specvol
    
    Following the coefficients conventions from the paper, which might be
    different from some libraries.
    
    * feat: Adding gsw_specvol_sso_0()
    
    * feat: Adding gsw_enthalpy_sso_0()
    
    * refactor: Moving constants into separated files
    
    * fix: Package name in lower case: gsw
    
    * refactor: Moving to no_std
    
    Do not rely on std so we can guarantee to run on Bare Metal, i.e.
    microcontrollers.
    
    * fix: Loading constants & coefficients in lib
    
    * doc: Adding description and repository to Cargo.toml
    
    * feat: Adding specific volume constants: A{ijk} & B{ijk}
    
    * Add CI checks (#2)
    
    * add CI
    
    * add readme
    
    * Add basic structure for FFI (#3)
    
    * add basic FFI infra
    
    * doc: Missing to define the license type
    
    * feat: Adding constants using the feature truncated
    
    Using a feature truncated allows to force the hardcoded truncated value
    used by other implementations thus permiting to validate our procedures
    with their values.
    
    * doc: Confoming/improving documentation style
    
    * feat: gsw_specvol_anom_standard()
    
    * feat: gsw_enthalpy_sso_0()
    
    * feat: gsw_specvol_alpha_beta()
    
    * feat: gsw_rho()
    
    * feat: gsw_z_from_p()
    
    * test: More unit tests
    test_gsw_specvol_anom_standard_at_standard(), test_specvol_vs_specvol_sso_0(), test_const_sfac()
    
    * feat: Declaring more constants
    
    * style: gsw_z_from_p()
    
    * style: Making easier to read too many digits
    
    * doc: Expanding title
    
    * doc: Configuring Zenodo's DOI
    
    * style: Unecessary return & cleaner scientific notation
    
    * style: Unecessary return
    
    Co-authored-by: Luiz Irber <luizirber@users.noreply.github.com>

---
+++ include/gswteos-10.h
@@ -0,0 +1,16 @@
+/* GSW TEOS-10 V3.05 definitions and prototypes. */
+
+#ifndef GSWTEOS_10_H
+#define GSWTEOS_10_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+
+double gsw_specvol(double sa, double ct, double p);
+
+#ifdef __cplusplus
+} // extern "C"
+#endif // __cplusplus
+
+#endif /* GSWTEOS_10_H */
---
+++ src/gsw_internal_const.rs
@@ -0,0 +1,40 @@
+/// ! GSW TEOS10 Constants
+
+/// Pi
+pub const PI: f64 = 3.141592653589793;
+
+pub const DEG2RAD: f64 = PI / 180.0;
+
+/// If the graviational acceleration were to be regarded as being
+/// depth-independent, which is often the case in ocean models, then gamma
+/// would be set to be zero here, and the code below works perfectly well.
+pub const GAMMA: f64 = 2.26e-7;
+
+/// Specific Heat [ J/(kg K) ]
+/// cp0: The "specific heat" for use with Conservative Temperature
+pub const GSW_CP0: f64 = 3991.86795711963;
+
+/// Conversion factor deciBar to Pascal
+pub const DB2PA: f64 = 1.0e4;
+
+/// SSO: Standard Ocean Reference Salinity [ g/kg ].
+pub const GSW_SSO: f64 = 35.16504;
+pub const GSW_SQRTSSO: f64 = 5.930011804372737;
+
+/// uPS: unit conversion factor for salinities [ g/kg ]
+pub const GSW_UPS: f64 = GSW_SSO / 35.0;
+
+#[cfg(feature = "truncated")]
+/// Other implementations hardcoded some constants truncating its values
+/// sfac  =  1/(40*gsw_ups) ~ 0.0248826675584615
+pub const GSW_SFAC: f64 = 0.0248826675584615;
+
+#[cfg(not(feature = "truncated"))]
+/// sfac  =  1/(40*gsw_ups) = 0.024882667558461472
+/// Two extra digits on precision compared with other implementations. The
+/// difference should be negligible but it impacts the validation tests.
+pub const GSW_SFAC: f64 = 1.0 / (40.0 * GSW_UPS);
+
+/// deltaS = 24, offset = deltaS*gsw_sfac = 5.971840214030754e-1
+// pub const OFFSET: f64 = 24.0 * GSW_SFAC;
+pub const OFFSET: f64 = 5.971840214030754e-1;
