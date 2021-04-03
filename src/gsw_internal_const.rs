10674578e933101f2c1c9ef1c44fda70a79fced3
Guilherme Castelão <guilherme@castelao.net>
2021-04-02 22:15:33 -0700

    feat: Declaring more constants

--- src/gsw_internal_const.rs
+++ src/gsw_internal_const.rs
@@ -1,5 +1,19 @@
 /// ! GSW TEOS10 Constants
 
+/// Pi
+pub const PI: f64 = 3.141592653589793;
+
+pub const DEG2RAD: f64 = PI / 180.0;
+
+/// If the graviational acceleration were to be regarded as being
+/// depth-independent, which is often the case in ocean models, then gamma
+/// would be set to be zero here, and the code below works perfectly well.
+pub const GAMMA: f64 = 2.26e-7;
+
+/// Specific Heat [ J/(kg K) ]
+/// cp0: The "specific heat" for use with Conservative Temperature
+pub const GSW_CP0: f64 = 3991.86795711963;
+
 /// Conversion factor deciBar to Pascal
 pub const DB2PA: f64 = 1.0e4;
 
@@ -20,3 +34,7 @@ pub const GSW_SFAC: f64 = 0.0248826675584615;
 /// Two extra digits on precision compared with other implementations. The
 /// difference should be negligible but it impacts the validation tests.
 pub const GSW_SFAC: f64 = 1.0 / (40.0 * GSW_UPS);
+
+/// deltaS = 24, offset = deltaS*gsw_sfac = 5.971840214030754e-1
+// pub const OFFSET: f64 = 24.0 * GSW_SFAC;
+pub const OFFSET: f64 = 5.971840214030754e-1;
