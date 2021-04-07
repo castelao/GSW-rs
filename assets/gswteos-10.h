670323111908599e06c582a4d52fa2bccf414402
Luiz Irber <luiz.irber@gmail.com>
2021-04-07 14:55:07 -0700

    add stubs for the C header

--- assets/gswteos-10.h
+++ assets/gswteos-10.h
@@ -9,6 +9,43 @@ extern "C" {
 
 double gsw_specvol(double sa, double ct, double p);
 
+void gsw_add_barrier(double *input_data,
+                     double lon,
+                     double lat,
+                     double long_grid,
+                     double lat_grid,
+                     double dlong_grid,
+                     double dlat_grid,
+                     double *output_data);
+
+void gsw_add_mean(double *data_in, double *data_out);
+
+double gsw_adiabatic_lapse_rate_from_ct(double sa, double ct, double p);
+
+double gsw_adiabatic_lapse_rate_ice(double t, double p);
+
+double gsw_alpha(double sa, double ct, double p);
+
+double gsw_alpha_on_beta(double sa, double ct, double p);
+
+double gsw_alpha_wrt_t_exact(double sa, double t, double p);
+
+double gsw_alpha_wrt_t_ice(double t, double p);
+
+double gsw_beta_const_t_exact(double sa, double t, double p);
+
+double gsw_beta(double sa, double ct, double p);
+
+double gsw_cabbeling(double sa, double ct, double p);
+
+double gsw_c_from_sp(double sp, double t, double p);
+
+double gsw_chem_potential_water_ice(double t, double p);
+
+double gsw_chem_potential_water_t_exact(double sa, double t, double p);
+
+double gsw_cp_ice(double t, double p);
+
 #ifdef __cplusplus
 } // extern "C"
 #endif // __cplusplus
--- src/ffi.rs
+++ src/ffi.rs
@@ -5,6 +5,95 @@ pub unsafe extern "C" fn gsw_specvol(sa: f64, ct: f64, p: f64) -> f64 {
     crate::gsw_specvol(sa, ct, p)
 }
 
+/*
+ * double *input_data, double lon, double lat,
+                double long_grid, double lat_grid, double dlong_grid,
+                double dlat_grid, double *output_data);
+*/
+#[no_mangle]
+pub unsafe extern "C" fn gsw_add_barrier(
+    input_data: *mut f64,
+    lon: f64,
+    lat: f64,
+    long_grid: f64,
+    lat_grid: f64,
+    dlong_grid: f64,
+    dlat_grid: f64,
+    output_data: *mut f64,
+) {
+    unimplemented!();
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn gsw_add_mean(data_in: *mut f64, data_out: *mut f64) {
+    unimplemented!();
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn gsw_adiabatic_lapse_rate_from_ct(sa: f64, ct: f64, p: f64) -> f64 {
+    unimplemented!();
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn gsw_adiabatic_lapse_rate_ice(t: f64, p: f64) -> f64 {
+    unimplemented!();
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn gsw_alpha(sa: f64, ct: f64, p: f64) -> f64 {
+    unimplemented!();
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn gsw_alpha_on_beta(sa: f64, ct: f64, p: f64) -> f64 {
+    unimplemented!();
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn gsw_alpha_wrt_t_exact(sa: f64, t: f64, p: f64) -> f64 {
+    unimplemented!();
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn gsw_alpha_wrt_t_ice(t: f64, p: f64) -> f64 {
+    unimplemented!();
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn gsw_beta_const_t_exact(sa: f64, t: f64, p: f64) -> f64 {
+    unimplemented!();
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn gsw_beta(sa: f64, ct: f64, p: f64) -> f64 {
+    unimplemented!();
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn gsw_cabbeling(sa: f64, ct: f64, p: f64) -> f64 {
+    unimplemented!();
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn gsw_c_from_sp(sp: f64, t: f64, p: f64) -> f64 {
+    unimplemented!();
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn gsw_chem_potential_water_ice(t: f64, p: f64) -> f64 {
+    unimplemented!();
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn gsw_chem_potential_water_t_exact(sa: f64, t: f64, p: f64) -> f64 {
+    unimplemented!();
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn gsw_cp_ice(t: f64, p: f64) -> f64 {
+    unimplemented!();
+}
+
 #[cfg(test)]
 mod test {
     use super::std::format;
