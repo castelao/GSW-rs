917b603e962db46d71f681ae28b198c8cbd1cf92
Luiz Irber <luizirber@users.noreply.github.com>
2021-05-02 01:49:19 +0000

    fix: only use thiserror on std (#11)

--- src/error.rs
+++ src/error.rs
@@ -1,25 +1,11 @@
-cfg_if::cfg_if! {
-if #[cfg(feature = "std")] {
-  extern crate std;
+#[cfg(feature = "std")]
+extern crate std;
 
-  #[derive(thiserror::Error, Debug)]
-  pub enum Error {
-    #[error("Negative salinity")]
-    NegativeSalinity
-  }
-
-  impl From<&str> for Error {
-    fn from(other: &str) -> Error {
-      match other {
-        "Negative SA" => Error::NegativeSalinity,
-        _ => unimplemented!()
-      }
-    }
-  }
-
-} else {
-  pub type Error = &'static str;
-}
+#[cfg_attr(feature = "std", derive(thiserror::Error))]
+#[derive(Debug)]
+pub enum Error {
+    #[cfg_attr(feature = "std", error("Negative salinity"))]
+    NegativeSalinity,
 }
 
 pub type Result<T> = core::result::Result<T, Error>;
