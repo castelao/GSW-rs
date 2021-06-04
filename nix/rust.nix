4c939e6281e95e5991398fcce21112dbf8bba46c
Luiz Irber <luizirber@users.noreply.github.com>
2021-06-04 01:25:11 +0000

    add nix configuration (#12)

---
+++ nix/rust.nix
@@ -0,0 +1,16 @@
+# nix/rust.nix
+{ sources ? import ./sources.nix }:
+let
+  pkgs =
+    import sources.nixpkgs { overlays = [ (import sources.rust-overlay) ]; };
+    #rustVersion = pkgs.rust-bin.stable."1.37.0".rust.override {
+    rustVersion = pkgs.rust-bin.stable.latest.rust.override {
+    #extensions = [ "rust-src" ];
+    #targets = [ "x86_64-unknown-linux-musl" ];
+    targets = [ "wasm32-wasi" "wasm32-unknown-unknown" "wasm32-unknown-emscripten"];
+  };
+in
+pkgs.makeRustPlatform {
+  cargo = rustVersion;
+  rustc = rustVersion;
+}
