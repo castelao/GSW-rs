4670a4ff6cbf6393a6dabfd6ae82ed5b8cceb694
Guilherme Castelão <guilherme@castelao.net>
2021-08-08 23:44:24 -0700

    feat: Moving to heapless objects
    This allows using postcard with no_std. Still a WIP.

--- validation/src/main.rs
+++ validation/src/main.rs
@@ -1,47 +1,59 @@
+// use core::ops::Deref;
+use heapless::{FnvIndexMap, String, Vec};
+use postcard::{from_bytes, to_vec};
+use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 
-/*
-{
-  "p_ref": [1,2,3, 4],
-  "lat_ref": [ 20, 30 m40]
-  "lon_ref": [180, 90, 34],
-  "specvol": [
-    [1, 2, 3, 4],
-    [10, 20, 30, 40],
-    [100, 200, NaN, NaN]
-    ]
-}
-*/
-
-struct Data {
-    p_ref: Vec<f64>,
-    specvol: Vec<Vec<f64>>,
-}
-
 fn main() {
-    let data: Data = serde_json::from_reader(file);
-    let pdata: Vec<u8> = postcard::to_vec(&data);
+    #[derive(Serialize, Deserialize, Debug)]
+    struct DataRef {
+        version: String<8>,
+        data: FnvIndexMap<String<8>, Vec<f64, 8>, 4>,
+    }
 
-    let mut dataset = HashMap::new();
+    let mut data = FnvIndexMap::<_, _, 4>::new();
 
-    let p: Vec<Vec<f64>> = vec![vec![
-        0.0, 10., 20., 30., 40., 50., 76., 101., 126., 151., 176., 202.,
-    ]];
-    dataset.insert("p".to_string(), p);
+    let p: Vec<f64, 8> = Vec::from_slice(&[0.0, 10., 20., 30., 40., 50., 76., 101.]).unwrap();
+    data.insert(String::from("p"), p).unwrap();
 
-    let sa: Vec<Vec<f64>> = vec![vec![
-        34.4682, 34.4981, 34.5066, 34.5387, 34.5398, 34.5375, 34.7361, 34.9891, 35.1204, 35.1193,
-    ]];
-    dataset.insert("sa".to_string(), sa);
+    let sa: Vec<f64, 8> = Vec::from_slice(&[
+        34.4682, 34.4981, 34.5066, 34.5387, 34.5398, 34.5375, 34.7361, 34.9891,
+    ])
+    .unwrap();
+    data.insert(String::from("sa"), sa).unwrap();
 
-    let ct: Vec<Vec<f64>> = vec![vec![
-        27.9964, 27.9939, 27.9440, 27.9484, 27.8838, 27.7933, 26.9473, 25.4643, 23.3798, 20.6385,
-        18.1439,
-    ]];
-    dataset.insert("ct".to_string(), ct);
+    let ct: Vec<f64, 8> = Vec::from_slice(&[
+        27.996436412058223,
+        27.993857176639093,
+        27.944017615967987,
+        27.948372399994330,
+        27.883806497845711,
+        27.793319415508925,
+        26.947345421342511,
+        25.464306249345810,
+    ])
+    .unwrap();
+    data.insert(String::from("ct"), ct).unwrap();
 
-    let specvol: Vec<Vec<f64>> = vec![vec![
-        0.9786, 0.9785, 0.9785, 0.9784, 0.9783, 0.9783, 0.9778, 0.9770, 0.9762, 0.9754, 0.9748,
-    ]];
-    dataset.insert("specvol".to_string(), specvol);
+    let specvol: Vec<f64, 8> = Vec::from_slice(&[
+        0.000978582446643,
+        0.000978520259453,
+        0.000978458407133,
+        0.000978396896780,
+        0.000978335707723,
+        0.000978268875230,
+        0.000977764021427,
+        0.000977040353397,
+    ])
+    .unwrap();
+    data.insert(String::from("specvol"), specvol).unwrap();
+
+    let dataset = DataRef {
+        version: String::from("3.06.12"),
+        data,
+    };
+
+    dbg!("{:?}", dataset);
+
+    // let test: Vec<u8, 64> = to_vec(&dataset).expect("Failed to vectorize dataset");
 }
