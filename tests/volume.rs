a6db5217233f515c2bbd46c13a37b07fd654dcee
Guilherme Castelão <guilherme@castelao.net>
2021-08-21 05:12:06 -0700

    test: Validating more volume functions
    Just to create more cases before generalizing it.

--- tests/volume.rs
+++ tests/volume.rs
@@ -15,6 +15,89 @@ struct DataRef {
 }
 
 #[test]
+fn alpha() {
+    let mut input = File::open("tests/data/gsw_validation.bin").expect("Unable to open file");
+    let mut contents = vec![];
+    input
+        .read_to_end(&mut contents)
+        .expect("Failed to read content");
+
+    let out: DataRef = from_bytes(&contents).unwrap();
+
+    let p = out.data2d.get(&String::from("p_chck_cast")).unwrap();
+    let sa = out.data2d.get(&String::from("SA_chck_cast")).unwrap();
+    let ct = out.data2d.get(&String::from("CT_chck_cast")).unwrap();
+    let alpha = out.data2d.get(&String::from("alpha")).unwrap();
+    for i in 0..3 {
+        for j in 0..45 {
+            if !alpha[i][j].is_nan() {
+                assert!(
+                    (gsw::volume::alpha(sa[i][j], ct[i][j], p[i][j]).unwrap() - alpha[i][j]).abs()
+                        <= f64::EPSILON
+                );
+            }
+        }
+    }
+}
+
+#[test]
+fn beta() {
+    let mut input = File::open("tests/data/gsw_validation.bin").expect("Unable to open file");
+    let mut contents = vec![];
+    input
+        .read_to_end(&mut contents)
+        .expect("Failed to read content");
+
+    let out: DataRef = from_bytes(&contents).unwrap();
+
+    let p = out.data2d.get(&String::from("p_chck_cast")).unwrap();
+    let sa = out.data2d.get(&String::from("SA_chck_cast")).unwrap();
+    let ct = out.data2d.get(&String::from("CT_chck_cast")).unwrap();
+    let beta = out.data2d.get(&String::from("beta")).unwrap();
+    for i in 0..3 {
+        for j in 0..45 {
+            if !beta[i][j].is_nan() {
+                assert!(
+                    (gsw::volume::beta(sa[i][j], ct[i][j], p[i][j]).unwrap() - beta[i][j]).abs()
+                        <= f64::EPSILON
+                );
+            }
+        }
+    }
+}
+
+#[test]
+fn rho() {
+    let mut input = File::open("tests/data/gsw_validation.bin").expect("Unable to open file");
+    let mut contents = vec![];
+    input
+        .read_to_end(&mut contents)
+        .expect("Failed to read content");
+
+    let out: DataRef = from_bytes(&contents).unwrap();
+
+    let p = out.data2d.get(&String::from("p_chck_cast")).unwrap();
+    let sa = out.data2d.get(&String::from("SA_chck_cast")).unwrap();
+    let ct = out.data2d.get(&String::from("CT_chck_cast")).unwrap();
+    let rho = out.data2d.get(&String::from("rho")).unwrap();
+    let tol = if cfg!(feature = "compat") || (f64::EPSILON > 1e-12) {
+        f64::EPSILON
+    } else {
+        1e-12
+    };
+    for i in 0..3 {
+        for j in 0..45 {
+            if !rho[i][j].is_nan() {
+                assert!(
+                    (gsw::volume::rho(sa[i][j], ct[i][j], p[i][j]).unwrap() - rho[i][j]).abs()
+                        <= tol
+                );
+            }
+        }
+    }
+}
+
+#[test]
 fn specvol() {
     let mut input = File::open("tests/data/gsw_validation.bin").expect("Unable to open file");
     let mut contents = vec![];
