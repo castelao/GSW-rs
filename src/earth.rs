0b1d098fa28405216125117b7fe6dca36f08f3ca
Guilherme Castelão <guilherme@castelao.net>
2021-09-25 10:13:37 -0700

    feat: earth::gravity()

--- src/earth.rs
+++ src/earth.rs
@@ -1,6 +1,7 @@
 //! Planet Earth properties
 
-use crate::gsw_internal_const::DEG2RAD;
+use crate::conversions::z_from_p;
+use crate::gsw_internal_const::{DEG2RAD, GAMMA};
 use crate::{Error, Result};
 
 /// Coriolis parameter (f)
@@ -18,4 +19,34 @@ pub fn coriollis_parameter(lat: f64) -> Result<f64> {
 
     let omega = 7.292115e-5;
     Ok(2.0 * omega * libm::sin(lat * DEG2RAD))
+}
+
+/// Gravity in the ocean
+///
+/// Calculates acceleration due to gravity as a function of latitude and as a
+/// function of pressure in the ocean.
+///
+/// # Example:
+/// ```
+/// use gsw::earth::gravity;
+/// let g = gravity(-18.0, 1250.0).unwrap();
+/// assert!((g - 9.78799904835888).abs() <= f64::EPSILON);
+/// ```
+///
+/// # Notes:
+/// * Here we use the convention that the height z, corresponding to the given
+///   pressure p, is negative in the ocean.
+/// * If feature nodgdz is activated, GAMMA is zero, thus this can be
+///   simplified.
+pub fn gravity(lat: f64, p: f64) -> Result<f64> {
+    if (lat < -90.0) | (lat > 90.0) {
+        return Err(Error::InvalidValue);
+    }
+
+    let sinlat = libm::sin(lat * DEG2RAD);
+    let sin2 = sinlat * sinlat;
+    let gs = 9.780327 * (1.0 + (5.2792e-3 + (2.32e-5 * sin2)) * sin2);
+
+    let z = z_from_p(p, lat, 0.0, 0.0);
+    Ok(gs * (1.0 - GAMMA * z))
 }
