33d42476c6e58b1b0c89c33d981d4d41ab7ab72f
Guilherme Castelão <guilherme@castelao.net>
2021-12-23 18:28:36 -0800

    fixed compilation on windows (bcrypt), and update nix deps

--- nix/rust.nix
+++ nix/rust.nix
@@ -4,7 +4,7 @@ let
   pkgs =
     import sources.nixpkgs { overlays = [ (import sources.rust-overlay) ]; };
     #rustVersion = pkgs.rust-bin.stable."1.37.0".rust.override {
-    rustVersion = pkgs.rust-bin.stable.latest.rust.override {
+    rustVersion = pkgs.rust-bin.stable.latest.default.override {
     #extensions = [ "rust-src" ];
     #targets = [ "x86_64-unknown-linux-musl" ];
     targets = [ "wasm32-wasi" "wasm32-unknown-unknown" "wasm32-unknown-emscripten"];
