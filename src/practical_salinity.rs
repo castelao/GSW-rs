84c7ec5ffafa255e7a524a5d07bfd92e0cc7cc06
Guilherme Castelão <guilherme@castelao.net>
2022-01-09 21:02:55 -0800

    feat: Reproducing NaN behavior of MatLab
    Reproduce MatLab behavior when S_p is negative, as well as when input is
    NaN.

--- src/practical_salinity.rs
+++ src/practical_salinity.rs
@@ -53,6 +53,21 @@ mod test_sp_from_c {
     }
 
     #[test]
+    // NaN input results in NaN output.
+    // Other libraries using GSW-rs might rely on this behavior to propagate
+    // and handle invalid elements.
+    fn nan() {
+        let sp = sp_from_c(f64::NAN, 1.0, 1.0);
+        assert!(sp.unwrap().is_nan());
+
+        let sp = sp_from_c(1.0, f64::NAN, 1.0);
+        assert!(sp.unwrap().is_nan());
+
+        let sp = sp_from_c(1.0, 1.0, f64::NAN);
+        assert!(sp.unwrap().is_nan());
+    }
+
+    #[test]
     // MatLab returns NaN if Rt < 0
     fn negative_cndc() {
         let sp = sp_from_c(-0.1, 10.0, 100.0);
@@ -158,10 +173,13 @@ pub fn sp_from_r(r: f64, t90: f64, p: f64) -> Result<f64> {
             / (1.0 + D1 * t68 + D2 * t68 * t68 + (D3 + D4 * t68) * r);
     let rt = r / (rp * rt_lc);
 
-    // Matlab returns NaN if rt < 0
-    // C: returns GSW_INVALID_VALUE if rt < 0
+    // if rt < 0, Matlab returns NaN and C returns GSW_INVALID_VALUE
     if rt < 0.0 {
-        return Ok(f64::NAN);
+        if cfg!(feature = "invalidasnan") {
+            return Ok(f64::NAN);
+        } else {
+            return Err(Error::Undefined);
+        }
     }
 
     let rtx = libm::sqrt(rt);
@@ -183,12 +201,19 @@ pub fn sp_from_r(r: f64, t90: f64, p: f64) -> Result<f64> {
         sp = hill_ratio * sp_hill_raw;
     }
 
-    // This line ensures that SP is non-negative.
     if sp < 0.0 {
-        sp = f64::NAN;
+        // MatLab forces zero if negative.
+        if cfg!(feature = "compat") {
+            Ok(0.0)
+        // C lib returns GSW_INVALID_VALUE instead
+        } else if cfg!(feature = "invalidasnan") {
+            Ok(f64::NAN)
+        } else {
+            Err(Error::NegativeSalinity)
+        }
+    } else {
+        Ok(sp)
     }
-
-    Ok(sp)
 }
 
 /// Conductivity ratio from Practical Salinity
