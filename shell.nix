292520893ae3362b6e26d89a6645f328a43b8699
Luiz Irber <luiz.irber@gmail.com>
2022-02-05 15:58:11 -0800

    nix and build updates

--- shell.nix
+++ shell.nix
@@ -6,24 +6,31 @@ in
   with pkgs;
 
   pkgs.mkShell {
+    nativeBuildInputs = [
+      clang_13
+    ];
+
     buildInputs = [
       rustPlatform.rust.cargo
+      openssl
+      pkg-config
+
+      llvmPackages_13.libclang
+      llvmPackages_13.libcxxClang
+
       git
-      stdenv.cc.cc.lib
-      llvmPackages.libclang
-      cmake
-      rust-bindgen
+      gdb
+
       cargo-watch
       cargo-c
       cargo-asm
-      gdb
-      (python39.withPackages(ps: with ps; [ virtualenv setuptools mypy ]))
+
+      (python39.withPackages(ps: with ps; [ virtualenv setuptools mypy tox ]))
     ];
 
-    shellHook = ''
-       # workaround for https://github.com/NixOS/nixpkgs/blob/48dfc9fa97d762bce28cc8372a2dd3805d14c633/doc/languages-frameworks/python.section.md#python-setuppy-bdist_wheel-cannot-create-whl
-       export SOURCE_DATE_EPOCH=315532800 # 1980
-       export LD_LIBRARY_PATH="${stdenv.cc.cc.lib}/lib64:$LD_LIBRARY_PATH";
-       export LIBCLANG_PATH="${llvmPackages_11.libclang.lib}/lib";
-    '';
+    BINDGEN_EXTRA_CLANG_ARGS = "-isystem ${llvmPackages_13.libclang.lib}/lib/clang/${lib.getVersion clang}/include";
+    LIBCLANG_PATH = "${llvmPackages_13.libclang.lib}/lib";
+
+    # workaround for https://github.com/NixOS/nixpkgs/blob/48dfc9fa97d762bce28cc8372a2dd3805d14c633/doc/languages-frameworks/python.section.md#python-setuppy-bdist_wheel-cannot-create-whl
+    SOURCE_DATE_EPOCH = 315532800; # 1980
   }
