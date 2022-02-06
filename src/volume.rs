911dbf86aca496c99f1cda69185042f8f77ea2b0
Guilherme Castelão <guilherme@castelao.net>
2022-02-06 06:31:15 -0800

    refactor: Reordering functions according to TEOS-10 card

--- src/volume.rs
+++ src/volume.rs
@@ -28,6 +28,104 @@ fn non_dimensional_sa(sa: f64) -> Result<f64> {
     Ok(libm::sqrt(GSW_SFAC * sa + OFFSET))
 }
 
+/// Specific volume of sea water (75-term polynomial approximation)
+///
+/// Calculates specific volume from Absolute Salinity, Conservative
+/// Temperature and pressure, using the computationally-efficient
+/// polynomial expression for specific volume (Roquet et al., 2014).
+///
+/// # Arguments
+///
+/// * `sa`: Absolute Salinity \[ g kg-1 \]
+/// * `ct`: Conservative Temperature (ITS-90) \[ deg C \]
+/// * `p`: sea pressure \[ dbar \] (i.e. absolute pressure - 10.1325 dbar)
+///
+/// # Returns
+///
+/// * `specvol`: specific volume \[m3 kg-1\]
+///
+/// Note that the coefficients v(i,j,k) follow the convention in the original
+/// paper, which is different from the convention used in the C-library.
+///
+pub fn specvol(sa: f64, ct: f64, p: f64) -> Result<f64> {
+    let xs: f64 = non_dimensional_sa(sa)?;
+    let ys: f64 = ct / GSW_CTU;
+    let z: f64 = non_dimensional_p(p);
+
+    // Specific Volume
+    Ok(V000
+        + xs * (V100 + xs * (V200 + xs * (V300 + xs * (V400 + xs * (V500 + xs * V600)))))
+        + ys * (V010
+            + xs * (V110 + xs * (V210 + xs * (V310 + xs * (V410 + xs * V510))))
+            + ys * (V020
+                + xs * (V120 + xs * (V220 + xs * (V320 + xs * V420)))
+                + ys * (V030
+                    + xs * (V130 + xs * (V230 + xs * V330))
+                    + ys * (V040
+                        + xs * (V140 + xs * V240)
+                        + ys * (V050 + xs * V150 + ys * V060)))))
+        + z * (V001
+            + xs * (V101 + xs * (V201 + xs * (V301 + xs * (V401 + xs * V501))))
+            + ys * (V011
+                + xs * (V111 + xs * (V211 + xs * (V311 + xs * V411)))
+                + ys * (V021
+                    + xs * (V121 + xs * (V221 + xs * V321))
+                    + ys * (V031
+                        + xs * (V131 + xs * V231)
+                        + ys * (V041 + xs * V141 + ys * V051))))
+            + z * (V002
+                + xs * (V102 + xs * (V202 + xs * (V302 + xs * V402)))
+                + ys * (V012
+                    + xs * (V112 + xs * (V212 + xs * V312))
+                    + ys * (V022
+                        + xs * (V122 + xs * V222)
+                        + ys * (V032 + xs * V132 + ys * V042)))
+                + z * (V003
+                    + xs * (V103 + xs * V203)
+                    + ys * (V013 + xs * V113 + ys * V023)
+                    + z * (V004 + xs * V104 + ys * V014 + z * (V005 + z * V006))))))
+}
+
+#[cfg(test)]
+mod test_specvol {
+    use super::specvol;
+
+    #[test]
+    // NaN input results in NaN output.
+    // Other libraries using GSW-rs might rely on this behavior to propagate
+    // and handle invalid elements.
+    fn nan() {
+        let v = specvol(f64::NAN, 1.0, 1.0);
+        assert!(v.unwrap().is_nan());
+
+        let v = specvol(1.0, f64::NAN, 1.0);
+        assert!(v.unwrap().is_nan());
+
+        let v = specvol(1.0, 1.0, f64::NAN);
+        assert!(v.unwrap().is_nan());
+    }
+
+    #[test]
+    // Test value from Roquet 2015 Appendix C.3, rounded to 9.732819628e-04
+    fn roquet2015() {
+        assert!((specvol(30., 10., 1000.0).unwrap() - 9.732819628e-04).abs() <= 5e-14);
+    }
+
+    #[allow(clippy::excessive_precision)]
+    #[test]
+    fn test_specvol() {
+        if cfg!(feature = "compat") {
+            // Test value from C library.
+            assert!(
+                (specvol(34.507499465692057, 27.994827331978655, 0.0).unwrap()
+                    - 0.00097855432330275953)
+                    .abs()
+                    < f64::EPSILON
+            );
+        }
+    }
+}
+
 /// in-situ density
 ///
 /// Calculates in-situ density from Absolute Salinity and Conservative
@@ -471,104 +569,6 @@ mod test_specvol_second_derivatives {
                 Err(Error::NegativeSalinity) => (),
                 _ => assert!(false),
             }
-        }
-    }
-}
-
-/// Specific volume of sea water (75-term polynomial approximation)
-///
-/// Calculates specific volume from Absolute Salinity, Conservative
-/// Temperature and pressure, using the computationally-efficient
-/// polynomial expression for specific volume (Roquet et al., 2014).
-///
-/// # Arguments
-///
-/// * `sa`: Absolute Salinity \[g kg-1\]
-/// * `ct`: Conservative Temperature (ITS-90) \[deg C\]
-/// * `p`: sea pressure \[dbar\] (i.e. absolute pressure - 10.1325 dbar)
-///
-/// # Returns
-///
-/// * `specvol`: specific volume \[m3 kg-1\]
-///
-/// Note that the coefficients v(i,j,k) follow the convention in the original
-/// paper, which is different from the convention used in the C-library.
-///
-pub fn specvol(sa: f64, ct: f64, p: f64) -> Result<f64> {
-    let xs: f64 = non_dimensional_sa(sa)?;
-    let ys: f64 = ct / GSW_CTU;
-    let z: f64 = non_dimensional_p(p);
-
-    // Specific Volume
-    Ok(V000
-        + xs * (V100 + xs * (V200 + xs * (V300 + xs * (V400 + xs * (V500 + xs * V600)))))
-        + ys * (V010
-            + xs * (V110 + xs * (V210 + xs * (V310 + xs * (V410 + xs * V510))))
-            + ys * (V020
-                + xs * (V120 + xs * (V220 + xs * (V320 + xs * V420)))
-                + ys * (V030
-                    + xs * (V130 + xs * (V230 + xs * V330))
-                    + ys * (V040
-                        + xs * (V140 + xs * V240)
-                        + ys * (V050 + xs * V150 + ys * V060)))))
-        + z * (V001
-            + xs * (V101 + xs * (V201 + xs * (V301 + xs * (V401 + xs * V501))))
-            + ys * (V011
-                + xs * (V111 + xs * (V211 + xs * (V311 + xs * V411)))
-                + ys * (V021
-                    + xs * (V121 + xs * (V221 + xs * V321))
-                    + ys * (V031
-                        + xs * (V131 + xs * V231)
-                        + ys * (V041 + xs * V141 + ys * V051))))
-            + z * (V002
-                + xs * (V102 + xs * (V202 + xs * (V302 + xs * V402)))
-                + ys * (V012
-                    + xs * (V112 + xs * (V212 + xs * V312))
-                    + ys * (V022
-                        + xs * (V122 + xs * V222)
-                        + ys * (V032 + xs * V132 + ys * V042)))
-                + z * (V003
-                    + xs * (V103 + xs * V203)
-                    + ys * (V013 + xs * V113 + ys * V023)
-                    + z * (V004 + xs * V104 + ys * V014 + z * (V005 + z * V006))))))
-}
-
-#[cfg(test)]
-mod test_specvol {
-    use super::specvol;
-
-    #[test]
-    // NaN input results in NaN output.
-    // Other libraries using GSW-rs might rely on this behavior to propagate
-    // and handle invalid elements.
-    fn nan() {
-        let v = specvol(f64::NAN, 1.0, 1.0);
-        assert!(v.unwrap().is_nan());
-
-        let v = specvol(1.0, f64::NAN, 1.0);
-        assert!(v.unwrap().is_nan());
-
-        let v = specvol(1.0, 1.0, f64::NAN);
-        assert!(v.unwrap().is_nan());
-    }
-
-    #[test]
-    // Test value from Roquet 2015 Appendix C.3, rounded to 9.732819628e-04
-    fn roquet2015() {
-        assert!((specvol(30., 10., 1000.0).unwrap() - 9.732819628e-04).abs() <= 5e-14);
-    }
-
-    #[allow(clippy::excessive_precision)]
-    #[test]
-    fn test_specvol() {
-        if cfg!(feature = "compat") {
-            // Test value from C library.
-            assert!(
-                (specvol(34.507499465692057, 27.994827331978655, 0.0).unwrap()
-                    - 0.00097855432330275953)
-                    .abs()
-                    < f64::EPSILON
-            );
         }
     }
 }
