bc5c8d56677cc605631638cafbb049981ea62344
Gui Castelao <guilherme@castelao.net>
2026-04-25 21:43:46 -0600

    cfg: Allow entropy_part as dead_code for now

--- src/gsw_internal_funcs/mod.rs
+++ src/gsw_internal_funcs/mod.rs
@@ -1268,7 +1268,7 @@ mod test_gibbs_ice {
 ///
 /// Reference:
 /// * TEOS-10 Manual and official GSW-C source (gsw_entropy_part).
-#[allow(clippy::excessive_precision)]
+#[allow(clippy::excessive_precision, dead_code)]
 pub(crate) fn entropy_part(sa: f64, t: f64, p: f64) -> Result<f64> {
     let sa = sanitize_salinity(sa)?;
     let x2 = GSW_SFAC * sa;
